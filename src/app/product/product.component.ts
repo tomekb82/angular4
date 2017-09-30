90687b5298b6de604e2f7bea36d1f62c27313108
Tomasz Belina <tomasz.belina@qualent.eu>
2017-09-30 15:28:02 +0200

    kosmetyka

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,24 +1,26 @@
 import {Component, Input} from '@angular/core';
-
-export interface IProduct{
-      name: string,
-      price:number,
-      description:string,
-      promoted: boolean,
-      tags: string
-}
+import {IProduct} from './product.repository';
 
 @Component({
     selector: 'product',
     template: `
-        <div [ngStyle] = "{background:product.promoted ? 'yellow': 'white'}">
+        <div class="w3-card-4" style="width:50%; float:left">
+          <img src="http://via.placeholder.com/300x100" alt={{product.name}} style="width:100%">
+          <div class="w3-container w3-center" [ngStyle] = "{background:product.promoted ? 'yellow': 'white'}">
             <h2>{{product.name}}, Price:{{product.price}} pln</h2>
-      	    <p>{{product.description}}</p>
-      	    <p>Tags: {{product.tags}}</p>
+            <p>{{product.description}}</p>
+            <p>Tags: {{product.tags}}</p>
+          </div>
         </div>
+
     `
 })
 export class ProductComponent {
     // To receive data we use Input annotation
     @Input() public product:IProduct;
 }
+/*<div [ngStyle] = "{background:product.promoted ? 'yellow': 'white'}">
+            <h2>{{product.name}}, Price:{{product.price}} pln</h2>
+            <p>{{product.description}}</p>
+            <p>Tags: {{product.tags}}</p>
+        </div>*/
